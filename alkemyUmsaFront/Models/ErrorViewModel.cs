ba24eb4e49c3a4320f8bc7afc9fe4345fc8308e1
FranciscoAndreoli <franandreoli7@gmail.com>
2023-09-25 09:04:23 -0300

    Add project files.

---
+++ alkemyUmsaFront/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace alkemyUmsaFront.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
