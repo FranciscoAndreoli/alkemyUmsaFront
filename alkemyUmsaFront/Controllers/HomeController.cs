a38fd03f38a03ae00a02481c3a7bf029dd65b712
FranciscoAndreoli <franandreoli7@gmail.com>
2023-09-30 15:36:12 -0300

    first commit.

--- alkemyUmsaFront/Controllers/HomeController.cs
+++ alkemyUmsaFront/Controllers/HomeController.cs
@@ -18,11 +18,6 @@ namespace alkemyUmsaFront.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ alkemyUmsaFront/Controllers/LoginController.cs
@@ -0,0 +1,16 @@
+﻿using Data.DTO;
+using Microsoft.AspNetCore.Mvc;
+namespace alkemyUmsaFront.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Ingresar(LoginDto dto)
+        {
+            return View("~/Views/Home/Index.cshtml");
+        }
+    }
+}
